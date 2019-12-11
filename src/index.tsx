89c5fd1d494069dc359b84e8fbedae5d8b30b544
Taylor Lapeyre <taylorlapeyre@gmail.com>
2019-12-10 20:19:37 -0800

    lol

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,60 @@
 // ReactDOM.render(<App />, document.getElementById('root'));
 
-const canvasHeight = window.innerHeight;
+export const canvasHeight = window.innerHeight;
 const canvasWidth = window.innerWidth;
 
 
-const input = "OZPZZ";
-const rules = { "OZ": "F-Z", "P": "F+", "F+Z": "-P+", "-": "FZ", "Z": "PF", "FF": "F" };
+const input = "X";
+const rules: any = {
+  "X": "+YF-XFX-FY+",
+  "Y": "-XF+YFY+FX-"
+};
 const alpha = 90;
-const iterations = 100;
-const r = 100
+const iterations = 10;
+const r = 50
+
+// const input = "FFF-FF-F-F+F+FF-F-FFF";
+// const rules = { "X": "Y" };
+// const alpha = 90;
+// const iterations = 1;
+// const r = 30
+
 
 let output = input;
 
-for (let i = 0; i < iterations; i++) {
-  for (const [inputRule, outputRule] of Object.entries(rules)) {
-    output = output.replace(inputRule, i => outputRule)
+function solveSystem(input: any, rules: any) {
+  let output = "";
+
+  let i = 0;
+  let j = 0;
+
+  const ruleKeys = Object.keys(rules);
+
+  while (i < input.length && j < input.length) {
+    console.log(i, j);
+
+    j = i;
+
+    while (
+      ruleKeys.some(ruleKey => ruleKey.startsWith(input.slice(i, j + 1))) &&
+      j < input.length
+    ) {
+      j = j + 1;
+    }
+
+    const match = input.slice(i, j);
+    output += rules[match];
+    console.log(output);
+
+    i = j;
   }
+
+  return output;
 }
 
-
-
+for (let i = 0; i < iterations; i++) {
+  output = solveSystem(output, rules);
+}
 
 const c = document.getElementsByTagName("canvas")[0];
 c.width = canvasWidth * 2;
@@ -28,22 +63,27 @@ const ctx = c.getContext("2d");
 
 
 if (ctx) {
+  // setup canvas
   ctx.lineWidth = 4;
   ctx.fillStyle = "f4f4f4";
   ctx.fillRect(0, 0, c.width, c.height);
 
-  let location = [canvasWidth, canvasHeight / 2];
+  // set start position and direction
+  let location = [canvasWidth / 2, canvasHeight / 2];
   let direction = alpha;
+
+  // set colors
   const strokeStyles = ["ffac8e", "fd7792", "3f4d71", "55ae95"];
   let currentColor = 0;
+
   ctx.moveTo(location[0], location[1]);
 
   for (const instruction of output.split("")) {
     console.log(instruction);
+
     switch (instruction) {
       case "+": {
         direction = direction + alpha;
-        console.log("Direction is now", direction)
         break;
       }
       case "-": {
@@ -52,8 +92,10 @@ if (ctx) {
       }
       case "F": {
         const nextX = location[0] + (r * Math.cos(direction * Math.PI / 180));
-        const nextY = location[1] + (r * Math.sin(direction * Math.PI / 180));
+        const nextY = location[1] - (r * Math.sin(direction * Math.PI / 180));
         const nextLocation = [nextX, nextY];
+
+        console.log(nextLocation)
 
         ctx.beginPath();
         ctx.moveTo(location[0], location[1]);
@@ -63,7 +105,6 @@ if (ctx) {
 
         currentColor = (currentColor + 1) % strokeStyles.length;
         location = nextLocation
-        console.log("Location is now", location)
         break;
       }
     }
